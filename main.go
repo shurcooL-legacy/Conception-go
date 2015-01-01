shurcooL-legacy/Conception-go

commit 6e6d9e8ab9f231e7c993721cd1ec2c95eb631ded

Minor refactor to use io.Copy over WriteTo/ReadFrom.

main.go
@@ -7204,12 +7204,12 @@ func main() {
 							r.ReadBytes('\n')
 						}
 						var b bytes.Buffer
-						b.ReadFrom(r)
+						io.Copy(&b, s.Stdin)
 						if b.Len() == 0 {
 							return nil
 						}
 						b.Truncate(b.Len() - 1)
-						b.WriteTo(s.Stdout)
+						io.Copy(s.Stdout, &b)
 						return nil
 					}),
 				))
@@ -7345,9 +7345,9 @@ func main() {
 						pipe_util.ExecCombinedOutput("go", "build", "-o", "/dev/null", goPackage.Bpkg.ImportPath),
 						pipe.TaskFunc(func(s *pipe.State) error {
 							var b bytes.Buffer
-							b.ReadFrom(s.Stdin)
+							io.Copy(&b, s.Stdin)
 							SetViewGroup(buildOutput, b.String())
-							b.WriteTo(s.Stdout)
+							io.Copy(s.Stdout, &b)
 							return nil
 						}),
 					))
@@ -7373,9 +7373,9 @@ func main() {
 							pipe_util.ExecCombinedOutput("go", "build", "-o", con2RunBinPath, goPackage.Bpkg.ImportPath),
 							pipe.TaskFunc(func(s *pipe.State) error {
 								var b bytes.Buffer
-								b.ReadFrom(s.Stdin)
+								io.Copy(&b, s.Stdin)
 								SetViewGroup(buildOutput, b.String())
-								b.WriteTo(s.Stdout)
+								io.Copy(s.Stdout, &b)
 								return nil
 							}),
 						),

commit d55d69df65081f1321e6b168364cd4347111add8

Fix bug introduced in 6e6d9e8ab9f231e7c993721 refactor.
Accidentally used wrong variable.

main.go
@@ -7204,7 +7204,7 @@ func main() {
 							r.ReadBytes('\n')
 						}
 						var b bytes.Buffer
-						io.Copy(&b, s.Stdin)
+						io.Copy(&b, r)
 						if b.Len() == 0 {
 							return nil
 						}