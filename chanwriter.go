6713ee74cdaa1fbab5518c204900b0e92c750c95
Dmitri Shuralyov <shurcooL@gmail.com>
2016-04-01 01:57:35 -0700

    Fix io.Writer implementation retaining p.
    Doing so is not allowed. This change makes the io.Writer implementation
    correct (resolving a TODO in code).

--- chanwriter.go
+++ chanwriter.go
@@ -3,7 +3,9 @@ package main
 type ChanWriter chan []byte
 
 func (cw ChanWriter) Write(p []byte) (n int, err error) {
-	// TODO: Copy the slice contents rather than sending the original, as it may get modified after we return?
-	cw <- p
-	return len(p), nil
+	// Make a copy of p in order to avoid retaining it.
+	b := make([]byte, len(p))
+	copy(b, p)
+	cw <- b
+	return len(b), nil
 }
